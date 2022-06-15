78c7af5e0c44efa1d883ec81b8b15d4ad289a826
simon <simonhchen@gmail.com>
2022-06-14 23:03:31 -0700

    added gitignore and completed lesson 6

--- space_rocks/game.py
+++ space_rocks/game.py
@@ -8,6 +8,7 @@ class SpaceRocks():
         # Initialize pygame and set the title
         pygame.init()
         pygame.display.set_caption("Space Rock")
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((800,600))
         self.background = load_sprite("space", False)
@@ -45,3 +46,5 @@ class SpaceRocks():
         if self.ship.collides_width(self.rock):
             self.collision_count += 1
             print(f"Collision #{self.collision_count}")
+
+        self.clock.tick(30)
